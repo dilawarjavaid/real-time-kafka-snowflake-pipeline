9f062f4741e095c54859aa50a70b6a58f7f84a79
Dilawar Javaid <dilawar.javaid864@gmail.com>
2026-09-01 16:31:06 +0500

    Fix local and Streamlit Cloud credential loading

--- dashboard/app.py
+++ dashboard/app.py
@@ -21,15 +21,29 @@ if st.button("🔄 Refresh Data"):
     st.cache_data.clear()
     st.rerun()
 
-
 @st.cache_resource
 def get_connection():
+
+    try:
+        account = st.secrets["SNOWFLAKE_ACCOUNT"]
+        user = st.secrets["SNOWFLAKE_USER"]
+        password = st.secrets["SNOWFLAKE_PASSWORD"]
+        warehouse = st.secrets["SNOWFLAKE_WAREHOUSE"]
+        database = st.secrets["SNOWFLAKE_DATABASE"]
+
+    except Exception:
+        account = os.getenv("SNOWFLAKE_ACCOUNT")
+        user = os.getenv("SNOWFLAKE_USER")
+        password = os.getenv("SNOWFLAKE_PASSWORD")
+        warehouse = os.getenv("SNOWFLAKE_WAREHOUSE")
+        database = os.getenv("SNOWFLAKE_DATABASE")
+
     return snowflake.connector.connect(
-        account=os.getenv("SNOWFLAKE_ACCOUNT"),
-        user=os.getenv("SNOWFLAKE_USER"),
-        password=os.getenv("SNOWFLAKE_PASSWORD"),
-        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
-        database=os.getenv("SNOWFLAKE_DATABASE"),
+        account=account,
+        user=user,
+        password=password,
+        warehouse=warehouse,
+        database=database,
         schema="GOLD"
     )
 
@@ -119,7 +133,7 @@ else:
         "Open-Meteo API → Python Producer → Apache Kafka → "
         "Snowflake Bronze → Stream + Task → Silver → Gold → Streamlit"
     )
-    
+
     st.subheader("Hourly Weather Data")
 
     st.dataframe(
